b3d819066322f40770094876bbf3c672b98f7b69
thanhpe <nguyenthanh120223@gmail.com>
2023-10-02 15:55:41 +0700

    Add project files.

---
+++ starup/Components/MenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using starup.Models;
+
+namespace starup.Compoments
+{
+    [ViewComponent(Name = "MenuView")]
+    public class MenuViewComponent : ViewComponent
+    {
+        private DataContext _context;
+        public MenuViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listofMenu = (from m in _context.Menus 
+                              where (m.IsActive == true) && (m.Position == 1) 
+                              select m).ToList();
+
+            //m.position == 1 là những menu nằm phía trên
+            //m.Position == 2 là những menu nằm phía dưới
+
+            return await Task.FromResult((IViewComponentResult)View("Default", listofMenu));
+        }
+    }
+}
---
+++ starup/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace starup.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Menu> Menus{ get; set; }
+    }
+}
